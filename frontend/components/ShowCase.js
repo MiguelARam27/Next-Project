210bb93089d0b1cf76dd98abe583bee8fa398af8
MiguelARam27 <garandster27@live.com>
2021-10-19 03:05:24 -0400

    ShowCase component

---
+++ frontend/components/ShowCase.js
@@ -0,0 +1,10 @@
+import styles from '@/styles/ShowCase.module.css';
+
+export default function ShowCase() {
+  return (
+    <div className={styles.showcase}>
+      <h1> Welcome to party</h1>
+      <h2>Find the hottest dj Events!</h2>
+    </div>
+  );
+}
