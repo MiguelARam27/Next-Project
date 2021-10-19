6b1866eeaf0cf394cea4e56ae898ad4f125f3089
MiguelARam27 <garandster27@live.com>
2021-10-19 00:40:24 -0400

    Added about page to layout

--- frontend/components/Layout.js
+++ frontend/components/Layout.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import styles from '../styles/Layout.module.css';
 import Header from './Header';
+import Footer from './Footer';
 export default function Layout({ title, keywords, description, children }) {
   return (
     <div>
@@ -11,6 +12,7 @@ export default function Layout({ title, keywords, description, children }) {
       </Head>
       <Header />
       <div className={styles.container}>{children}</div>
+      <Footer />
     </div>
   );
 }
--- frontend/pages/about.js
+++ frontend/pages/about.js
@@ -1,7 +1,10 @@
+import Layout from '../components/Layout';
 export default function AboutPage() {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <Layout>
+      <div>
+        <h2>about page</h2>
+      </div>
+    </Layout>
   );
 }
