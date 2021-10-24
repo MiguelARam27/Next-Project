3729345b515f1e877b1b685cf196f0d674481266
MiguelARam27 <garandster27@live.com>
2021-10-23 23:46:11 -0400

    Auth context initialized

---
+++ frontend/context/AuthContext.js
@@ -0,0 +1,36 @@
+import { createContext, useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { NEXT_URL } from '@/config/index';
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState(null);
+
+  //Register a user
+
+  const register = async (user) => {
+    console.log(user);
+  };
+  //login a user
+
+  const login = async ({ email: identifier, password }) => {
+    console.log(user);
+  };
+  //logut a user
+  const logout = async () => {
+    console.log('logout');
+  };
+  //check if user is logged in
+
+  const checkUserLoggedIn = async (user) => {
+    console.log('logout');
+  };
+  return (
+    <AuthContext.Provider value={{ user, error, register, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
