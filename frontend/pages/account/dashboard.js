9ff97884bdf58d344aa0e68793255b76cd14627f
MiguelARam27 <garandster27@live.com>
2021-10-24 21:11:05 -0400

    get users events

--- frontend/pages/account/dashboard.js
+++ frontend/pages/account/dashboard.js
@@ -1,6 +1,24 @@
 import React from 'react';
 import Layout from '@/components/Layout';
+import { parseCookies } from '@/helpers/index';
+import { API_URL } from '@/config/index';
 
-export default function DashboardPage() {
+export default function DashboardPage({ events }) {
+  console.log(events);
   return <Layout title="Dashboard"></Layout>;
+}
+
+export async function getServerSideProps({ req }) {
+  const { token } = parseCookies(req);
+
+  const res = await fetch(`${API_URL}/events/me`, {
+    method: 'GET',
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  console.log(token);
+
+  const events = await res.json();
+  return {
+    props: { events },
+  };
 }
